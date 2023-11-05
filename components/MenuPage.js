e7f447b9e7a8ce0d4859b0ac20eac52074211b00
sharmistha071 <shormy.roy@gmail.com>
2023-11-05 20:44:08 +0000

    custom component added

---
+++ components/MenuPage.js
@@ -0,0 +1,25 @@
+export class MenuPage extends HTMLElement{
+    constructor(){
+        super()
+        this.root = this.attachShadow({mode: 'open'})
+        const styles = document.createElement("style")
+        this.root.appendChild(styles)
+
+        async function loadCSS(){
+           const rendercss =  await fetch("/components/MenuPage.css")
+           const css = await rendercss.text()
+
+        }
+        loadCSS()
+    }
+
+    // when the component is attached to the DOM
+    connectedCallback(){
+        const template = document.getElementById('menu-page-template')
+        const content = template.content.cloneNode(true)
+        this.root.appendChild(content)
+
+    }
+}
+
+customElements.define('menu-page', MenuPage)
